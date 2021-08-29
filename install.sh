0d62c1d481d539a843561653b34697009e6a6133
trzin2772 <89734807+trzin2772@users.noreply.github.com>
2021-08-29 11:38:25 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+npm i encodeurl
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+echo "━━━━━━━✦✗✦━━━━━━━━"
+echo "ɪɴsᴛᴀʟᴀᴄ̧ᴀ̃ᴏ ᴄᴏɴᴄʟᴜɪ́ᴅᴀ"
+echo "━━━━━━━✦✗✦━━━━━━━━"
+echo " "
+echo "          ⣠⣴⣶⣿⠿⢿⣶⣶⣦⣄ "
+echo "        ⠀⣼⡿⠋⠁⠀⠀⠀⢀⣈⠙⢿⣷⡄ "
+echo "       ⠀⢸⣿⠁⠀⢀⣴⣿⠿⠿⠿⠿⠿⢿⣷⣄"⠀
+echo "   ⢀⣀⣠⣾⣿⡇⠀⣾⣿⡄⠀⠀⠀⠀⠀⠀⠀⠹⣧ "
+echo "  ⣾⡿⠉⠉⣿⠀⡇⠀⠸⣿⡌⠓⠶⠤⣤⡤⠶⢚⣻⡟ "
+echo "  ⣿⣧⠖⠒⣿⡄⡇⠀⠀⠙⢿⣷⣶⣶⣶⣶⣶⢿⣿ "⠀
+echo "  ⣿⡇⠀⠀⣿⡇⢰⠀⠀⠀⠀⠈⠉⠉⠉⠁⠀⠀⣿ "
+echo "  ⣿⡇⠀⠀⣿⡇⠈⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿ "⠀
+echo "  ⣿⣷⠀⠀⣿⡇⠀⠘⠦⣄⣀⣀⣀⣀⣀⡤⠊⠀⣿ "⠀
+echo "  ⢿⣿⣤⣀⣿⡇⠀⠀⠀⢀⣀⣉⡉⠁⣀⡀  ⣾⡟ "
+echo "     ⠉⠛⠛⣿⡇⠀⠀⠀⠀⣿⡟⣿⡟⠋⠀ ⣿⠃"
+echo "        ⠀⣿⣧⠀⠀⠀⢀⣿⠃⣿⣇⠀⢀⣸⡯ "⠀⠀
+echo "        ⠀⠹⢿⣶⣶⣶⠿⠃⠀⠈⠛⠛⠛⠛⠁ "
+echo " "
+echo "[ ISABOT ] -> um dos bots mais avançados atualmente. digite \"npm start\" para iniciar."
